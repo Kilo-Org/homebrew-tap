20c34b25b610f282870dc1d0ef89d66613ec87e3
Brendan O'Leary <brendan@olearycrew.com>
2026-02-03 22:09:15 -0500

    Downgrade Kilocode version from 1.0.13 to 1.0.12

--- kilocode.rb
+++ kilocode.rb
@@ -4,8 +4,8 @@
 class Kilocode < Formula
   desc "The most complete CLI for agentic engineering."
   homepage "https://github.com/Kilo-Org/kilo"
-  version "1.0.13"
-  url "https://registry.npmjs.org/@kilocode/cli/-/cli-1.0.13.tgz"
+  version "1.0.12"
+  url "https://registry.npmjs.org/@kilocode/cli/-/cli-1.0.12.tgz"
   sha256 "be8d5b7dd45881b6e4014c0a635bdd71be34e2e"
   license "MIT"
 
