cada4813781104bd1b7a85524aee09898edc6206
Brendan O'Leary <brendan@olearycrew.com>
2026-02-03 22:08:54 -0500

    Update sha256 checksum for kilocode version 1.0.13

--- kilocode.rb
+++ kilocode.rb
@@ -6,7 +6,7 @@ class Kilocode < Formula
   homepage "https://github.com/Kilo-Org/kilo"
   version "1.0.13"
   url "https://registry.npmjs.org/@kilocode/cli/-/cli-1.0.13.tgz"
-  sha256 "be8d5b7dd45881b6e4014c0a635bdd71be34e2e5887a7746461b488c4af61841"
+  sha256 "be8d5b7dd45881b6e4014c0a635bdd71be34e2e"
   license "MIT"
 
   depends_on "node"
